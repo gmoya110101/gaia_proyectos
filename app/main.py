acc8b00c50b728c3985ab74ff358a49274321aaa
BOCIO <juanmecanico@gmail.com>
2026-04-16 21:33:41 -0600

    feat: Mi primer commit

--- app/main.py
+++ app/main.py
@@ -1,23 +1,11 @@
 """Main module for the Gaia Proyectos application."""
 
-
-# Por fa borra estas funciones y pon tu código aquí, no olvides importar lo que necesites
-def algo():
-    """A placeholder function for future implementation."""
-    pass
-
-def another_function():
-    """Another placeholder function for future implementation."""
-
-    pass
-
-##############################################################################
-
+def saludo():
+    print ("hola mundo")
 def main():
     """Entry point for the Gaia Proyectos application."""
     #TODO: Put your magic code here
-    algo()
-    another_function()
-
+    saludo()
 if __name__ == "__main__":
     main()
+
