1e59d721b1944d6c75a3a8e4431557e3a92bf07c
Gerardo Iturribarria Moya <87717640+gmoya110101@users.noreply.github.com>
2026-04-21 21:03:51 -0600

    chore: Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 """Main module for the Gaia Proyectos application."""
 
-def saludo():
-    print ("hola mundo")
+def saludo(nombre: str):
+    print (f"!Hola, {nombre}")
 def main():
     """Entry point for the Gaia Proyectos application."""
     #TODO: Put your magic code here
-    saludo()
+    saludo("Gerardo")
+    
 if __name__ == "__main__":
     main()
-
-print
