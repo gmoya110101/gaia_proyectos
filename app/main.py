601c76be44fad0d4a34679e7ee79dd0f9e245b4e
Gerardo Iturribarria Moya <87717640+gmoya110101@users.noreply.github.com>
2026-04-21 21:19:22 -0600

    chore: Update main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ def saludo(nombre: str):
     print (f"¡Hola, {nombre}!")
 def main():
     """Entry point for the Gaia Proyectos application."""
-    #TODO: Put your magic code here
-    saludo("Gerardo")
+    #TODO: Pon tu código mágico aquí
     saludo("Christopher")   
 if __name__ == "__main__":
     main()
