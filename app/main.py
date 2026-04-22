8f350ad1b1782d844e4699c002afa0bc72ca650e
BOCIO <juanmecanico@gmail.com>
2026-04-21 21:18:36 -0600

    feat: Ultima modificacion

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 """Main module for the Gaia Proyectos application."""
 
 def saludo(nombre: str):
-    print (f"!Hola, {nombre}")
+    print (f"¡Hola, {nombre}!")
 def main():
     """Entry point for the Gaia Proyectos application."""
     #TODO: Put your magic code here
     saludo("Gerardo")
-    
+    saludo("Christopher")   
 if __name__ == "__main__":
     main()
+
