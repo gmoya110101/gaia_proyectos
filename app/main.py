f0d215cc664094db1a25d1099c1406d8db97f63e
BOCIO <juanmecanico@gmail.com>
2026-04-25 19:50:20 -0600

    feat: the new project progress

--- app/main.py
+++ app/main.py
@@ -5,7 +5,32 @@ def saludo(nombre: str):
 def main():
     """Entry point for the Gaia Proyectos application."""
     #TODO: Pon tu código mágico aquí
-    saludo("Christopher")   
+def atleta_hibrido(nombre:str, edad:int, ocupacion:str):
+    
+    usuario= {
+        "nombre":nombre,
+        "edad": edad,
+        "ocupacion": ocupacion
+
+    }
+    usuario ["activo"]= True
+    return usuario
+
+def main():
+    print("--- Registro de Atleta Híbrido ---")
+
+    try:
+
+        nombre_usuario= str(input("COLOCA TU NOMBRE"))
+        edad_usuario= int(input("COLOCA TU EDAD"))
+        ocupacion_usuario= str(input("COLOCA TU OCUPACION"))
+        resultado= atleta_hibrido(nombre_usuario, edad_usuario, ocupacion_usuario)
+        print("\nRegistro exitoso:")
+        print(resultado)
+
+    except:
+        print("Error: La edad debe ser un número.")
+
+# ¡No olvides el interruptor de encendido al final!
 if __name__ == "__main__":
     main()
-
