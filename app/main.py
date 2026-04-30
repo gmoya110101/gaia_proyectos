e1a676bef62e43cd78fdd1e44ba473f80edad53f
GITURRIBARRIAM <jsotogon@tessa.com.uy>
2026-04-29 21:13:53 -0600

    feat: remaster main code.

--- app/main.py
+++ app/main.py
@@ -1,36 +1,34 @@
 """Main module for the Gaia Proyectos application."""
 
-def saludo(nombre: str):
-    print (f"¡Hola, {nombre}!")
-def main():
-    """Entry point for the Gaia Proyectos application."""
-    #TODO: Pon tu código mágico aquí
-def atleta_hibrido(nombre:str, edad:int, ocupacion:str):
-    
-    usuario= {
-        "nombre":nombre,
-        "edad": edad,
-        "ocupacion": ocupacion
 
-    }
-    usuario ["activo"]= True
+def insert_persistent_data(nombre: str, edad: int, ocupacion: str):
+
+    usuario = {"nombre": nombre, "edad": edad, "ocupacion": ocupacion}
+    usuario["activo"] = True
     return usuario
 
-def main():
+
+def input_user_data() -> None:
     print("--- Registro de Atleta Híbrido ---")
 
     try:
 
-        nombre_usuario= str(input("COLOCA TU NOMBRE"))
-        edad_usuario= int(input("COLOCA TU EDAD"))
-        ocupacion_usuario= str(input("COLOCA TU OCUPACION"))
-        resultado= atleta_hibrido(nombre_usuario, edad_usuario, ocupacion_usuario)
+        nombre_usuario = str(input("COLOCA TU NOMBRE"))
+        edad_usuario = int(input("COLOCA TU EDAD"))
+        ocupacion_usuario = str(input("COLOCA TU OCUPACION"))
+        resultado = insert_persistent_data(
+            nombre_usuario, edad_usuario, ocupacion_usuario
+        )
         print("\nRegistro exitoso:")
         print(resultado)
 
     except:
         print("Error: La edad debe ser un número.")
 
-# ¡No olvides el interruptor de encendido al final!
+
+def main():
+    input_user_data()
+
+
 if __name__ == "__main__":
     main()
